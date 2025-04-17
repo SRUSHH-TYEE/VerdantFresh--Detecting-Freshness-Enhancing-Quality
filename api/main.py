3cdbcdccb78e0d9a72ff278dc2c812a27d96d2e5
SRUSHH-TYEE <nayaksrushti40@gmail.com>
2025-04-17 02:20:34 -0500

    added front end

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import requests
 
@@ -12,6 +13,20 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app=FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 endpoint="http://localhost:8501/v1/models/apple_model:predict"
 
 class_names=["Apple__Healthy","Aplle_Rotten"]
